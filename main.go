a762c3fdc31904fb261e73b4b23803f8889b0afd
vkehfdl1 <vkehfdl1@gmail.com>
2022-06-03 23:59:09 +0900

    default gRPC connection server

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	pb "fssn_grpc/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedMyServiceServer
+}
+
+func (s *server) MyHelloFunction(ctx context.Context, in *pb.MyNumber) (*pb.MyNumber, error) {
+	log.Printf("Received: %d", in.GetValue())
+	return &pb.MyNumber{Value: in.GetValue() * in.GetValue()}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterMyServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
